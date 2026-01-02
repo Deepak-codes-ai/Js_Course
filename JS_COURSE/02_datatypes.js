44a8133416594dce5ea7615e090d9ebd4961e7aa
Deepak Thakur <dc789741@gmail.com>
2026-01-02 05:37:29 +0000

    datatypes

---
+++ JS_COURSE/02_datatypes.js
@@ -0,0 +1,19 @@
+"use Strict" //treat all JS code as newer version
+
+// alert("hello"); //we are using nodejs not browsers
+
+// Number => 2 to power 35
+//bigint
+// string=""
+//boolean => true/false;
+//null => standAlone value
+//undefined =>
+//symbol =>unique
+
+//objects
+
+
+let age = 18;
+let isGood = false;
+let state;
+console.log(typeof null, undefined,) //object , undefined

a2478b706afdf0d371deafc29aa65c21fc2843e6
Deepak Thakur <dc789741@gmail.com>
2026-01-02 16:57:34 +0000

    03 added

---
+++ JS_COURSE/03_conversion_oper.js
@@ -0,0 +1,53 @@
+let score1 = 33;
+let score = "33";
+
+console.log(typeof score1);
+let scoreInNumber = Number(score); //converted string in number
+console.log(typeof (scoreInNumber));
+
+console.log(scoreInNumber);
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let isLogedIn = "Deep";
+
+let boolisLoggedIn = Boolean(isLogedIn);
+console.log(boolisLoggedIn);
+
+
+let Stringg = 33;
+
+let StringNum = String(Stringg)
+console.log(StringNum)
+console.log(typeof StringNum)
+
+//true=1 ,, false=0;
+
+
+
+// *************************OPERATIONs**********************
+
+let value = 3;
+let NegValue = -value;
+console.log(NegValue);
+
+// console.log(2 + 3)
+// console.log(2 - 3)
+// console.log(2 % 3)
+// console.log(2 / 3)
+// console.log(2 * 3)
+// console.log(2 ** 3)
+
+let str1 = "Hello";
+let str2 = " Deep";
+
+let str3 = (str1 + str2);
+console.log(str3)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true) //true
+console.log(+true) //1
+console.log("") //0

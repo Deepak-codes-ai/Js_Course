73723f540b24cca2cd16120d5fd95f023a5db69f
Deepak Thakur <dc789741@gmail.com>
2026-01-02 05:22:17 +0000

    pckage json added for nodemon

---
+++ JS_COURSE/01_var.js
@@ -0,0 +1,11 @@
+const accountId = 12245; //value cannot be changed 
+
+let accountEmail = "deepchauhan@gmail.com";
+
+var name = "Deepak"
+accountCity = "Jhansi";
+
+// accountId = 2321; not allowed
+console.log(accountId);
+
+console.table([accountId, accountEmail, name, accountCity])

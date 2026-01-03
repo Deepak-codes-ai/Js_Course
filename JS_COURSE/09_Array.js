0943f0fe3a16f64151c12931128284364d6ff013
Deepak Thakur <dc789741@gmail.com>
2026-01-03 16:12:16 +0000

    updated

--- JS_COURSE/09_Array.js
+++ JS_COURSE/09_Array.js
@@ -55,3 +55,56 @@ console.log(Array.from("Deepak"));
 console.log(Array.from({ name: "Deepak" }));
 
 
+
+
+//******************METHODS***********************
+// forEach() – Just Loop (No Return)
+let numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbs.forEach(items => {
+    console.log(items * 2)
+});
+
+// map() – Transform Array 
+
+let squares = numbs.map(n => n * n);
+console.log(squares)
+
+// filter() – Select Some Elements
+
+let filtered = numbs.filter(items => items % 2 === 0);
+console.log(filtered)
+
+// reduce() – Reduce to One Value
+
+// array.reduce((accumulator, current) => {
+//   return updatedAccumulator;
+// }, initialValue);
+
+const cart = [
+    { item: "Mobile", price: 10000 },
+    { item: "iPhone", price: 40000 }
+]
+
+let sum = cart.reduce((total, n) => total + n.price, 0);
+console.log(sum);
+
+
+// find() – Find One Element
+
+let users = [
+    {
+        id: 1, name: "Deepak"
+    },
+    { id: 2, name: "Ram" }
+];
+
+let user = users.find(items => items.id === 2);
+console.log(user);
+
+// some() & every()
+
+let nums = [2, 4, 6];
+
+console.log(nums.some(n => n % 2 !== 0)); // false
+console.log(nums.every(n => n % 2 === 0)); // true

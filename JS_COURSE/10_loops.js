cff62c1535d709ffa1f5c2687ca1249e462bbfdd
Deepak Thakur <dc789741@gmail.com>
2026-01-07 18:08:11 +0000

    File updated

--- JS_COURSE/10_loops.js
+++ JS_COURSE/10_loops.js
@@ -4,14 +4,14 @@
 // code
 // }
 
-for (let i = 1; i <= 3; i++) {
-    if (i === 2) break;
-    console.log(i);
-}
-for (let i = 1; i <= 3; i++) {
-    if (i === 2) continue;
-    console.log(i);
-}
+// for (let i = 1; i <= 3; i++) {
+//     if (i === 2) break;
+//     console.log(i);
+// }
+// for (let i = 1; i <= 3; i++) {
+//     if (i === 2) continue;
+//     console.log(i);
+// }
 
 
 // while Loop
@@ -22,11 +22,11 @@ for (let i = 1; i <= 3; i++) {
 //     // code
 // }
 
-let i = 1;
-while (i <= 5) {
-    console.log(i);
-    i++;
-}
+// let i = 1;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
 
 
 
@@ -39,15 +39,85 @@ while (i <= 5) {
 // } while(condition);
 
 // Example:
-let j = 10;
-do {
-    console.log(j);
-    j++;
-} while (j <= 12)
+// let j = 10;
+// do {
+//     console.log(j);
+//     j++;
+// } while (j <= 12)
 
 
-for (let index = 1; index <= 10; index++) {
-    const element = `5 X ${index}=${5 * index}`;
-    console.log(element)
+// for (let index = 1; index <= 10; index++) {
+//     const element = `5 X ${index}=${5 * index}`;
+//     console.log(element)
 
-}
+// }
+
+//For of loops-------------------
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greeting = "Hello Deepak";
+// for (const greet of greeting) {
+//     console.log(greet);
+// }
+
+
+//Map method---------------- its used for unique value
+
+// const map = new Map();
+// map.set('IN', 'India');
+// map.set('USA', 'United State Of America');
+// // console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+// for in loop--------------------
+
+// const myObject = {
+//     game1: "NFS",
+//     game2: "spiderman",
+//     game3: "superman",
+// }
+// for (const key in myObject) {
+//     console.log(`${key} for ${myObject[key]}`);
+// }
+
+// ---------------------------------
+
+// const codings = ["C", "Java", "JS", "C++", "PHP"];
+
+// codings.forEach((items, index, arr) => {
+//     console.log(items, index, arr);
+// })
+
+// function printme(items) {
+//     console.log(items);
+// }
+// codings.forEach(printme)
+
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFile: "JS"
+    },
+    {
+        languageName: "Python",
+        languageFile: "Py"
+    },
+    {
+        languageName: "Java",
+        languageFile: "J"
+    },
+]
+
+myCoding.forEach((items) => {
+    console.log(items.languageFile);
+
+})
